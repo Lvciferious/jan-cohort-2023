11c3c1cb5b6bd98d627bc34071729eba49c4de71
akumaluna <akumaluna>
2023-01-23 15:27:00 -0700

    goodbye-tue

---
+++ mod-1-week-1/w1-tue-goodbye.js
@@ -0,0 +1,23 @@
+/*
+
+Goodbye (*)
+Write a function goodbye(name) that takes in a string name
+and returns a string saying bye to that name. See the examples below.
+
+*/
+
+function goodbye(name) {
+    console.log('bye ' + name + '.');
+    return name;
+}
+
+goodbye('Daniel');
+goodbye('Mark');
+goodbye('Beyonce');
+
+/*
+  console.log(goodbye("Daniel")); // => "Bye Daniel."
+  console.log(goodbye("Mark")); // => "Bye Mark."
+  console.log(goodbye("Beyonce")); // => "Bye Beyonce."
+
+*/
