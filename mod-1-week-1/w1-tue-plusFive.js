d24073f70ef907abbd98917cc0ed3ced90b60101
akumaluna <akumaluna>
2023-01-23 16:35:34 -0700

    plusFive-tue

---
+++ mod-1-week-1/w1-tue-plusFive.js
@@ -0,0 +1,24 @@
+/*
+
+Plus Five
+Write a function plusFive that takes in a number
+as an argument and returns the sum of that number and 5.
+
+*/
+
+function plusFive(num) {
+    let sum = num + 5;
+    console.log(sum);
+    return sum;
+  }
+
+
+  plusFive(10); //15
+  plusFive(2); //7
+  plusFive(-8); //-3
+
+  /*
+  console.log(plusFive(10));  // => 15
+  console.log(plusFive(2));   // => 7
+  console.log(plusFive(-8));  // => -3
+*/
