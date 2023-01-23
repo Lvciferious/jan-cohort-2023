b57e254fea460748e3a320a3dd798daeba6710d3
akumaluna <akumaluna>
2023-01-23 15:50:21 -0700

    muscle-memory-tue

---
+++ mod-1-week-1/w1-tue-muscle-memory.js
@@ -0,0 +1,30 @@
+/*
+
+Muscle Memory
+
+Practice typing the syntax for functions.
+Write 10 functions with different messages
+and console.log them. Focus on using proper
+style and indentation. Be sure to test your
+code by running it after each repetition.
+You'll be surprised and also annoyed at what
+details you missed! Learn from those typos
+now and make your life easier.
+
+*/
+
+function sayHello(greeting) {
+    console.log(greeting + '!');
+    return greeting;
+}
+
+sayHello('Guten tag');
+sayHello('Kumusta');
+sayHello('안녕하세요');
+sayHello('こんにちは');
+sayHello('Buenos tardes');
+sayHello('สวัสดี');
+sayHello('Bonjour');
+sayHello('Olá');
+sayHello('你好');
+sayHello('Hello');
