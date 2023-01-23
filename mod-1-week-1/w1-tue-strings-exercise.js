253894faa7875d093de7873a135748c4976effa5
akumaluna <akumaluna>
2023-01-23 14:11:56 -0700

    strings-exercise-tue

---
+++ mod-1-week-1/w1-tue-strings-exercise.js
@@ -0,0 +1,13 @@
+/*
+
+Strings Exercise
+Write 3 examples of expressions. One using + (concatenation),
+another using [] (indexing), and another using .length.
+Print out the result of your expressions using console.log.
+Be sure to test your work by running your code!
+
+*/
+
+console.log('I like ' + 'pie' + '!'); // 'I like pie!'
+console.log('celestial'.indexOf('i')); // 6
+console.log('celestial'.length); // 9
