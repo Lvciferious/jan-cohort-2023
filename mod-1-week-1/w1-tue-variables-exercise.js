ca89b0fcff32e70e3b197969a89d45f144ac1e34
akumaluna <akumaluna>
2023-01-23 11:56:09 -0700

    variables-exercise-tue

---
+++ mod-1-week-1/w1-tue-variables-exercise.js
@@ -0,0 +1,26 @@
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+
+
+// 3. Declare a variable called `age`:
+
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+
+// 5. Assign the `age` variable to a number:
+
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+
+const firstName = 'Lucifer'
+const lastName = 'Valdez Luna'
+const age = '31'
+
+console.log('FIRST NAME: ' + firstName + ' ' + 'LAST NAME: ' + lastName + ' ' + "AGE: " + age);
+console.log(firstName, lastName, age);
