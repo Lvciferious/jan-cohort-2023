1c92c8df4d61a82b57e952106ecb063d3d47f9a5
akumaluna <akumaluna>
2023-01-23 18:27:27 -0700

    hello-tue

---
+++ mod-1-week-1/w1-tue-hello.js
@@ -0,0 +1,24 @@
+/*
+
+Hello
+Write a function hello that takes in a string and prints out "Hello, " followed
+by the string.
+
+HINT: this function doesn't need to return anything, it should just print using console.log.
+
+*/
+
+function hello(str) {
+    console.log('Hello, ' + str);
+    return str;
+  }
+
+  hello('cat');
+  hello('Helen');
+  hello('Sting');
+
+  /*
+  hello("cat"); // prints "Hello, cat"
+  hello("Helen"); // prints "Hello, Helen"
+  hello("Sting"); // prints "Hello, Sting"
+*/
