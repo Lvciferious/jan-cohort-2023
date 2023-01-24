a520612cab7bc1d48d4bc7bdc93096677aaa15db
akumaluna <akumaluna>
2023-01-24 16:18:20 -0700

    log-between-tue

---
+++ mod-1-week-1/w1-tue-log-between.js
@@ -0,0 +1,20 @@
+/*
+
+Log Between
+Define a function logBetween(lowNum, highNum)
+that will print every number from lowNum to highNum, inclusive.
+Inclusive means that the range includes lowNum and highNum.
+
+Hint: this function only needs to print using console.log it does not need to return.
+
+*/
+
+function logBetween (num1, num2) {
+    for (let i = num1 ; i <= num2 ; i++) {
+      console.log(i);
+    }
+  }
+
+  logBetween(-1, 2); // prints out: -1 0 1 2
+  logBetween(14, 6); // => prints nothing
+  logBetween(4, 6); // prints out: 4 5 6
