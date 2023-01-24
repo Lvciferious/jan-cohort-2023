36f934839be6d7517b75d358089c79259d83a6d0
akumaluna <akumaluna>
2023-01-24 16:59:05 -0700

    stings-exercise-revisit-wed

---
+++ mod-1-week-1/w1-wed-strings-exercise-revisit.js
@@ -0,0 +1,21 @@
+/*
+
+Strings Exercise (Revisit)
+
+Write 3 examples of expressions.
+One using + (concatenation), another using [] (indexing),
+and another using .length.
+Print out the result of your expressions using console.log.
+Be sure to test your work by running your code!
+
+*/
+
+console.log('hello ' + 'world');  // 'hello world'
+
+// Write 3 more expressions below:
+
+console.log('I ' + 'like ' + 'pie' + '!');
+
+let chillAminalzzz = ['quokka', 'capybara', 'doge', 'shark'];
+console.log(chillAminalzzz);
+console.log(chillAminalzzz.length);
