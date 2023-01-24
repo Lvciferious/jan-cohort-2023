bbc89398b04abb0745f493a52a919f4c51dbf762
akumaluna <akumaluna>
2023-01-23 18:33:49 -0700

    div-by-three-tue

---
+++ mod-1-week-1/w1-divide-by-three.js
@@ -0,0 +1,17 @@
+/*
+
+Divide by Three
+ Write a function divideByThree(num) that returns the passed in
+number argument divided by three.
+
+*/
+
+function divideByThree(num) {
+    const quotient = num / 3
+    console.log(quotient);
+    return quotient;
+}
+
+divideByThree(9); // 3
+divideByThree(15); // 5
+divideByThree(98); //32.666
