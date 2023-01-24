b3f0c713fffe63d0ccb3bce4100e1231645b3149
akumaluna <akumaluna>
2023-01-23 18:39:26 -0700

    whisper-tue

---
+++ mod-1-week-1/w1-tue-whisper.js
@@ -0,0 +1,15 @@
+/*
+
+Whisper
+Write a function whisper that takes in a string and returns a "whispered" version of that string.
+
+HINT: try using the .toLowerCase() function on a string to see what it does!
+
+*/
+
+function whisper(str) {
+    console.log('...' + str.toLowerCase() + '...');
+}
+
+whisper('Hey Buddy');
+whisper('YEA! that was fun...');
