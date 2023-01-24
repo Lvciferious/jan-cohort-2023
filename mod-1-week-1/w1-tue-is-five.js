5f6c0e70b0968e77727093fb1acd675a993f2a21
akumaluna <akumaluna>
2023-01-23 19:53:54 -0700

    isFive-tue

---
+++ mod-1-week-1/w1-tue-is-five.js
@@ -0,0 +1,17 @@
+/*
+
+Is Five
+Define a function isFive that will return true
+if a number is equal to 5 and false if it is not.
+
+*/
+
+function isFive(num) {
+    if (num === 5) {
+        return true;
+}
+    return false;
+}
+
+console.log(isFive(5));
+console.log(isFive(13));
