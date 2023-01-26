61f47f65ae9e7867ee746de1a4bdb093ab9495c9
akumaluna <akumaluna>
2023-01-26 10:49:06 -0700

    update commit

--- mod-1-week-1/w1-tue-doubler.js
+++ mod-1-week-1/w1-tue-doubler.js
@@ -32,6 +32,11 @@ function doubler(arrayNum) {
 }
 
 
+console.log(doubler([1, 2, 3, 4])); // => [2, 4, 6, 8]
+console.log(doubler([7, 1, 8])); // => [14, 2, 16]
+
+
+
 function doubler(arrayNum) {
     let doubleArr = [];
 
