bf1a448f8afdce6627bdd235ff4f37ce08124116
akumaluna <akumaluna>
2023-01-26 11:31:26 -0700

    fizz-buzz-tue

---
+++ mod-1-week-1/w1-tue-fizz-buzz.js
@@ -0,0 +1,38 @@
+/*
+
+Fizz Buzz
+
+Define a function fizzBuzz(max) that takes a number and prints
+every number from 0 to max (not inclusive) that is divisible by either 3 or 5, but not
+both.
+
+*/
+
+//GAMEPLAN
+//what is a non-tech solution?
+//from 3 to 18, I would discard the nums that are not div by 3 || 5, and return what's left
+//tech-translation:
+//we need a for loop to iterate
+// exclusive conditional
+//if condish met return numbers
+
+// Examples:
+
+function fizzBuzz(max) {
+    for (let i = 0; i < max; i++) {
+    if ((i % 3 === 0 && i % 5 !== 0) || (i % 5 === 0 && i % 3 !== 0)) {
+        console.log(i);
+        }
+    }
+}
+
+(fizzBuzz(20)); // prints out:
+/*
+3
+5
+6
+9
+10
+12
+18
+*/
